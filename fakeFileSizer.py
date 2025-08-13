37916ca40cef4d857a66577ed09ea0236fc8e2c7
Shivam Kumar <82958158+shivamkumar15@users.noreply.github.com>
2025-08-13 20:58:15 +0530

    Add files via upload

---
+++ fakeFileSizer.py
@@ -0,0 +1,27 @@
+
+import os
+
+def pad_executable(input_file, output_file, target_size_mb):
+    target_size = target_size_mb * 1024 * 1024
+
+    input_size = os.path.getsize(input_file)
+    padding_size = target_size - input_size
+    
+    if padding_size <= 0:
+        print("! The input file's size is larger than the target size.")
+        return
+
+    with open(input_file, 'rb') as f_in:
+        with open(output_file, 'wb') as f_out:
+
+            f_out.write(f_in.read())
+
+            f_out.write(b'\0' * padding_size)
+    
+    print(f"* File '{output_file}' has been created with size {target_size_mb} MB.")
+
+
+in_file = input('Your input file name (for exmpl: test.exe):')
+out_file = input('Your output file name (for exmpl: test.exe):')
+num = int(input('Your desired size in mb:'))
+pad_executable(in_file, out_file, num)
